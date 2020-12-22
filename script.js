9bc665b38eea0de42510a4cf5b2b3ab105265fed
alderikBrennos <tristanranchon24@hotmail.fr>
2020-12-22 11:48:36 +0100

    nav

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+
+///Script pour menu nav
+var activeLi = document.getElementById("nav-Bar");
+var elLi = activeLi.getElementsByClassName("li");
+
+for (var i = 0; i < elLi.length; i++) {
+  elLi[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}

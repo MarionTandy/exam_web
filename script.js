fad26175c538d42bd85ebf0283bd10f89c89f151
Marion Tandy <tandymarion@gmail.com>
2020-12-23 09:58:01 +0100

    ajout menu mobile (Tristan)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 ///Script pour menu nav
 var activeLi = document.getElementById("nav-Bar");
 var elLi = activeLi.getElementsByClassName("li");
@@ -11,3 +10,31 @@ for (var i = 0; i < elLi.length; i++) {
     this.className += " active";
   });
 }
+
+
+///script pour menu mobile
+var activeLiMobile = document.getElementById("menuMobile");
+var elLiMobile = activeLiMobile.querySelectorAll("li");
+
+for (var i = 0; i < elLiMobile .length; i++) {
+  elLiMobile[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active-mobile");
+    current[0].className = current[0].className.replace("active-mobile", "");
+    this.className += "active-mobile";
+  });
+}
+
+
+///script pour le menu burger
+function showMenu() {
+  var x = document.getElementById("menuMobile");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var listeMobile = document.getElementById("btnMobile");
+
+listeMobile.addEventListener("click", showMenu);
